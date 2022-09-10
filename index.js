618db2fb091477a738e59a51c2374ca27c0192d7
SirJimKe <jimmymutuku1@gmail.com>
2022-09-10 05:52:41 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+  static sanitize(string) {
+    return string.replace(/[^A-Za-z0-9- ']+/g, '');
+  }
+  static titleize(string) {
+    const exclude = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from'];
+    const stringArray = string.split(" ");
+    return stringArray.map((word, i) => {
+      return exclude.includes(word) && i!==0 ? [word] : word.charAt(0).toUpperCase() + word.slice(1)
+    }).join(" ");
+  }
 }
